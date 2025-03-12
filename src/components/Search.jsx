104c8cb6126ffc1ae6dea4f0f5a13273f8b0f198
Emre Koç <kochhhemre@gmail.com>
2025-03-12 16:25:14 +0300

    small updates

---
+++ src/components/Search.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const person = {
+  name: "Emre Koç",
+  age: 31,
+  location: "Kayseri",
+};
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search though thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
